fcfe116b5093e700bcba7b4bc38e79f109f4020b
iago-monteirog <iagomgarcia9000@gmail.com>
2023-05-31 20:12:46 -0300

    WIP: aula 02

--- 02-ignite-timer/src/App.tsx
+++ 02-ignite-timer/src/App.tsx
@@ -1,18 +1,17 @@
 import { ThemeProvider } from 'styled-components'
 import { defaultTheme } from './styles/themes/default'
-import { Button } from './components/Button'
 import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
+
 
 export function App() {
   return (
-    <ThemeProvider theme={defaultTheme}>
-      <Button variant="primary" />
-      <Button variant="secondary" />
-      <Button variant="danger" />
-      <Button variant="success" />
-      <Button />
-
-      <GlobalStyle />
-    </ThemeProvider>
+    <BrowserRouter>
+      <ThemeProvider theme={defaultTheme}>
+        <Router />
+        <GlobalStyle />
+      </ThemeProvider>
+    </BrowserRouter>
   )
 }
