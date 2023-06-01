de1838f2f1d7fc842255b81ccf8403d2a0761069
iago-monteirog <iagomgarcia9000@gmail.com>
2023-06-01 20:27:33 -0300

    aula 02

--- 02-ignite-timer/src/layouts/DefaultLayout/styles.ts
+++ 02-ignite-timer/src/layouts/DefaultLayout/styles.ts
@@ -2,7 +2,7 @@ import styled from "styled-components";
 
 export const LayoutContainer = styled.div`
     max-width: 74rem;
-    height: calc(100vh - 10rem);
+    height: calc(140vh - 10rem);
     margin: 5rem;
     padding: 2.5rem;
 
