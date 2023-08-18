d4716cb4448e96c571227e98486722c083a7f25a
iago-monteirog <iagomgarcia9000@gmail.com>
2023-08-18 18:43:52 -0300

    01: add transaction page

--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,5 +1,7 @@
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
+import { SearchForm } from "./components/SearchForm";
+import * as S from './styles';
 
 export function Transactions() {
     return (
@@ -7,22 +9,33 @@ export function Transactions() {
             <Header />
             <Summary />
 
-            <table>
-                <tbody>
-                    <tr>
-                        <td width="50%">Desenvolvimento de site</td>
-                        <td>R$ 12.000,00</td>
-                        <td>Venda</td>
-                        <td>18/08/2023</td>
-                    </tr>
-                    <tr>
-                        <td width="50%">Hamburguer</td>
-                        <td>- R$ 59,00</td>
-                        <td>Venda</td>
-                        <td>18/08/2023</td>
-                    </tr>
-                </tbody>
-            </table>
+            <S.TransactionsContainer>
+                <SearchForm />
+                <S.TransactionsTable>
+                    <tbody>
+                        <tr>
+                            <td width="50%">Desenvolvimento de site</td>
+                            <td>
+                                <S.PriceHighLight variant="income">
+                                    R$ 12.000,00
+                                </S.PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>18/08/2023</td>
+                        </tr>
+                        <tr>
+                            <td width="50%">Hamburguer</td>
+                            <td>
+                                <S.PriceHighLight variant="outcome">
+                                    - R$ 59,00
+                                </S.PriceHighLight>
+                            </td>
+                            <td>Venda</td>
+                            <td>18/08/2023</td>
+                        </tr>
+                    </tbody>
+                </S.TransactionsTable>
+            </S.TransactionsContainer>
 
         </div>
     )
