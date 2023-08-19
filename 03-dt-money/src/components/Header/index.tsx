0cd29ee6af3b44622fe436d031a2a29f2b7bd777
iago-monteirog <iagomgarcia9000@gmail.com>
2023-08-19 16:53:19 -0300

    01 feat: new transaction modal

--- 03-dt-money/src/components/Header/index.tsx
+++ 03-dt-money/src/components/Header/index.tsx
@@ -1,12 +1,21 @@
 import * as S from './styles';
 import logoImg from '../../assets/logo.svg'
+import * as Dialog from '@radix-ui/react-dialog';
+import { NewTransactionModal } from '../NewTransactionModal';
 
 export function Header() {
     return (
         <S.HeaderContainer>
             <S.HeaderContent>
                 <img src={logoImg} alt="" />
-                <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+
+                <Dialog.Root>
+                    <Dialog.Trigger asChild>
+                        <S.NewTransactionButton>Nova transação</S.NewTransactionButton>
+                    </Dialog.Trigger>
+
+                    <NewTransactionModal />
+                </Dialog.Root>
             </S.HeaderContent>
         </S.HeaderContainer>
     )
