245454a49b231d04739fa7ca7a01015665afffd6
iago-monteirog <iagomgarcia9000@gmail.com>
2023-08-19 21:19:53 -0300

    03: create Transaction Context

--- 03-dt-money/src/components/Summary/index.tsx
+++ 03-dt-money/src/components/Summary/index.tsx
@@ -1,7 +1,24 @@
 import { ArrowCircleDown, ArrowCircleUp, CurrencyDollar } from 'phosphor-react';
 import * as S from './styles';
+import { useContext } from 'react';
+import { TransactionsContext } from '../../contexts/TransactionsContext';
 
 export function Summary() {
+    const { transactions } = useContext(TransactionsContext);
+
+    const summary = transactions.reduce((acc, transaction) => {
+        if (transaction.type === 'income') {
+            acc.income += transaction.price;
+            acc.total += transaction.price;
+        } else {
+            acc.outcome += transaction.price;
+            acc.total -= transaction.price;
+        }
+
+        return acc;
+    },
+        { income: 0, outcome: 0, total: 0 })
+
     return (
         <S.SummaryContainer>
             <S.SummaryCard>
@@ -10,7 +27,7 @@ export function Summary() {
                     <ArrowCircleUp size={32} color='#00b37e' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.income}</strong>
             </S.SummaryCard>
             <S.SummaryCard>
                 <header>
@@ -18,7 +35,7 @@ export function Summary() {
                     <ArrowCircleDown size={32} color='#f75a68' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.outcome}</strong>
             </S.SummaryCard>
             <S.SummaryCard variant='green'>
                 <header>
@@ -26,7 +43,7 @@ export function Summary() {
                     <CurrencyDollar size={32} color='#fff' />
                 </header>
 
-                <strong>R$ 17.400,00</strong>
+                <strong>{summary.total}</strong>
             </S.SummaryCard>
         </S.SummaryContainer>
     )
--- 03-dt-money/src/pages/Transactions/index.tsx
+++ 03-dt-money/src/pages/Transactions/index.tsx
@@ -1,9 +1,14 @@
+import { useContext } from "react";
 import { Header } from "../../components/Header";
 import { Summary } from "../../components/Summary";
 import { SearchForm } from "./components/SearchForm";
 import * as S from './styles';
+import { TransactionsContext } from "../../contexts/TransactionsContext";
+import { priceFormatter } from "../../utils/formatter";
 
 export function Transactions() {
+    const { transactions } = useContext(TransactionsContext);
+
     return (
         <div>
             <Header />
@@ -13,30 +18,24 @@ export function Transactions() {
                 <SearchForm />
                 <S.TransactionsTable>
                     <tbody>
-                        <tr>
-                            <td width="50%">Desenvolvimento de site</td>
-                            <td>
-                                <S.PriceHighLight variant="income">
-                                    R$ 12.000,00
-                                </S.PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>18/08/2023</td>
-                        </tr>
-                        <tr>
-                            <td width="50%">Hamburguer</td>
-                            <td>
-                                <S.PriceHighLight variant="outcome">
-                                    - R$ 59,00
-                                </S.PriceHighLight>
-                            </td>
-                            <td>Venda</td>
-                            <td>18/08/2023</td>
-                        </tr>
+                        {transactions.map((transaction => {
+                            return (
+                                <tr key={transaction.id}>
+                                    <td width="50%">{transaction.description}</td>
+                                    <td>
+                                        <S.PriceHighLight variant={transaction.type}>
+                                            {transaction.type === 'outcome' && '- '}
+                                            {priceFormatter.format(transaction.price)}
+                                        </S.PriceHighLight>
+                                    </td>
+                                    <td>{transaction.category}</td>
+                                    <td>{transaction.createdAt}</td>
+                                </tr>
+                            )
+                        }))}
                     </tbody>
                 </S.TransactionsTable>
             </S.TransactionsContainer>
-
         </div>
     )
 }
