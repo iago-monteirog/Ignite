22233548699728ee90da36c0dc987c7c1eb0e23d
iago-monteirog <iagomgarcia9000@gmail.com>
2023-09-25 12:06:26 -0300

    aula 03: criando usuário no adapter

--- 06-ignite-call/src/lib/auth/prisma-adapter.ts
+++ 06-ignite-call/src/lib/auth/prisma-adapter.ts
@@ -1,18 +1,57 @@
 /* eslint-disable @typescript-eslint/no-non-null-assertion */
+import { NextApiRequest, NextApiResponse } from 'next'
 import { Adapter } from 'next-auth/adapters'
+import { parseCookies, destroyCookie } from 'nookies'
 import { prisma } from '../prisma'
 
-export function PrismAdapter(): Adapter {
+export function PrismaAdapter(
+  req: NextApiRequest,
+  res: NextApiResponse,
+): Adapter {
   return {
-    // async createUser(user) {},
+    async createUser(user) {
+      const { '@ignitecall:userId': userIdOnCookies } = parseCookies({ req })
+
+      if (!userIdOnCookies) {
+        throw new Error('User ID not found on cookies.')
+      }
+
+      const prismaUser = await prisma.user.update({
+        where: {
+          id: userIdOnCookies,
+        },
+        data: {
+          name: user.name,
+          email: user.email,
+          avatar_url: user.avatar_url,
+        },
+      })
+
+      destroyCookie({ res }, '@ignitecall:userId', {
+        path: '/',
+      })
+
+      return {
+        id: prismaUser.id,
+        name: prismaUser.name,
+        username: prismaUser.username,
+        email: prismaUser.email!,
+        emailVerified: null,
+        avatar_url: prismaUser.avatar_url!,
+      }
+    },
 
     async getUser(id) {
-      const user = await prisma.user.findUniqueOrThrow({
+      const user = await prisma.user.findUnique({
         where: {
           id,
         },
       })
 
+      if (!user) {
+        return null
+      }
+
       return {
         id: user.id,
         name: user.name,
@@ -22,14 +61,17 @@ export function PrismAdapter(): Adapter {
         avatar_url: user.avatar_url!,
       }
     },
-
     async getUserByEmail(email) {
-      const user = await prisma.user.findUniqueOrThrow({
+      const user = await prisma.user.findUnique({
         where: {
           email,
         },
       })
 
+      if (!user) {
+        return null
+      }
+
       return {
         id: user.id,
         name: user.name,
@@ -39,9 +81,8 @@ export function PrismAdapter(): Adapter {
         avatar_url: user.avatar_url!,
       }
     },
-
     async getUserByAccount({ providerAccountId, provider }) {
-      const { user } = await prisma.account.findUniqueOrThrow({
+      const account = await prisma.account.findUnique({
         where: {
           provider_provider_account_id: {
             provider,
@@ -52,6 +93,12 @@ export function PrismAdapter(): Adapter {
           user: true,
         },
       })
+
+      if (!account) {
+        return null
+      }
+
+      const { user } = account
 
       return {
         id: user.id,
@@ -120,7 +167,7 @@ export function PrismAdapter(): Adapter {
     },
 
     async getSessionAndUser(sessionToken) {
-      const { user, ...session } = await prisma.session.findUniqueOrThrow({
+      const prismaSession = await prisma.session.findUnique({
         where: {
           session_token: sessionToken,
         },
@@ -128,6 +175,12 @@ export function PrismAdapter(): Adapter {
           user: true,
         },
       })
+
+      if (!prismaSession) {
+        return null
+      }
+
+      const { user, ...session } = prismaSession
 
       return {
         session: {
@@ -146,7 +199,7 @@ export function PrismAdapter(): Adapter {
       }
     },
 
-    async updateSession({ sessionToken, expires, userId }) {
+    async updateSession({ sessionToken, userId, expires }) {
       const prismaSession = await prisma.session.update({
         where: {
           session_token: sessionToken,
@@ -162,6 +215,14 @@ export function PrismAdapter(): Adapter {
         userId: prismaSession.user_id,
         expires: prismaSession.expires,
       }
+    },
+
+    async deleteSession(sessionToken) {
+      await prisma.session.delete({
+        where: {
+          session_token: sessionToken,
+        },
+      })
     },
   }
 }
